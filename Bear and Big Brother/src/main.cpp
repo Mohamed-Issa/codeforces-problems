0eda5bfb7a9a3fc849110993c94b79be92cf80d1
mohamed <mohamed.hamdy.issa@gmail.com>
2020-06-24 15:37:52 +0200

    "bear and big brother problem"

---
+++ Bear and Big Brother/src/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * main.cpp
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: mohamed
+ */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int a, b, INT { 0 };
+	cin >> a >> b;
+	while (a <= b) {
+		b = b * 2;
+		a = a * 3;
+		INT++;
+	}
+
+	cout << INT << endl;
+}
+
